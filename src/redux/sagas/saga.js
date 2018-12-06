f6c3e192fed57f6ea01f254b8bb7d118dcb33966
KrazhauRaman <krazhau.raman@gmail.com>
2018-12-06 12:28:03 +0300

    v.0.8.1
    Saga also creates breed pages with pics

--- src/redux/sagas/saga.js
+++ src/redux/sagas/saga.js
@@ -31,21 +31,27 @@ function* createBreedPagesAndFetchPhotoUrls() {
         const breedData = yield select(state => state.listOfBreeds.listOfBreeds);
         yield console.log(breedData)
 
-        for (let breed in breedData ) {
-            
+        // getting photoes for all breeds and  subbreeds
+        for (let breed in breedData) {
+
 
             if (breedData[breed].length > 0) {
 
                 yield console.log(breedData[breed])
 
-                for (let subBreed in breedData[breed] ) {
+                for (let subBreed in breedData[breed]) {
                     yield console.log(breed + " " + breedData[breed][subBreed])
+
+                    let name = breed + " " + breedData[breed][subBreed];
 
                     let breedPics = yield call(() => {
                         console.log(breed)
                         return getImagesOfDogs(breed, 5, breedData[breed][subBreed])
                     });
+
                     yield console.log(breedPics.message)
+
+                    yield put({ type: Actions.ADD_BREED_PAGE, breedPage: { name: name, breedPics: breedPics.message } });
                 }
             }
             else {
@@ -55,12 +61,14 @@ function* createBreedPagesAndFetchPhotoUrls() {
                     return getImagesOfDogs(breed, 5)
                 });
                 yield console.log(breedPics.message)
+
+                yield put({ type: Actions.ADD_BREED_PAGE, breedPage: { name: breed, breedPics: breedPics.message } });
             }
         }
 
         // yield put({ type: Actions.SET_ALL_BREEDS, listOfBreeds: breedData.message });
-        yield put({ type: Actions.ADD_BREED_PAGE, breedPage: 1 });
-        yield put({ type: Actions.ADD_BREED_PAGE, breedPage: 22 });      
+
+        yield put({ type: Actions.ADD_BREED_PAGE, breedPage: 22 });
 
 
 
